7aee6426ba5f1c2530b036c202662a6411113356
IZUMOHARU <57467675+IZUMOHARU@users.noreply.github.com>
2019-11-07 11:38:49 +0800

    期中完成！
    照著講義試著做了一下！

---
+++ UnityProject/Assets/Robot Kyle/npc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc : MonoBehaviour {
+
+    [Header("對話")]
+    public string Say = "Hello, hello. I am Near. Who are you?";
+    public string SaySpeed = "0.2";
+
+    [Header("任務")]
+    public bool Complete;
+    public string BateryFinish = "5";
+    public string Batery = "0";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
